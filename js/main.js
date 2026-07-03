f1a3f1051f59c7db45846ea4266f2d8bb2c86ac9
Who-Tma <whotma.dev@gmail.com>
2026-07-03 05:10:09 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -23,15 +23,15 @@ adsShowing.addEventListener('click',()=>{
     console.log('OkkkkAds');
 })
 
-// const tg = window.Telegram.WebApp;
-// const user = tg.initDataUnsafe?.user;
-// const a = user.id;
+ const tg = window.Telegram.WebApp;
+ const user = tg.initDataUnsafe?.user;
+ //const a = user.id;
 
 
 
 
 
-let user = {id:99998999,first_name:'aminAzizi2',is_premium:true,username:'@test2'};
+//let user = {id:99998999,first_name:'aminAzizi2',is_premium:true,username:'@test2'};
 const TelId = user.id;
 document.addEventListener('DOMContentLoaded', () => {
     async function UserFound() {
